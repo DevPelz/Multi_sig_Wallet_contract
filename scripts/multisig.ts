6ee2e18882d786da60495fa5ba6c67731ba30a96
Dev_Pelz <pelzfx1@gmail.com>
2023-09-06 19:07:50 +0100

    add: multisig.ts

---
+++ scripts/multisig.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [Admin1, Admin2, Admin3, Admin4] = await ethers.getSigners();
+    const Owners = [Admin1.address, Admin2.address, Admin3.address, Admin4.address];
+    const multisig = await ethers.deployContract('Multisig', [Owners],{
+        value: ethers.parseEther('10')
+    });
+
+    await multisig.waitForDeployment();
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
