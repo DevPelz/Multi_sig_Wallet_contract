93c109971f47ec339b3716abdf14628309997bb5
Dev_Pelz <pelzfx1@gmail.com>
2023-09-07 16:53:31 +0100

    modified: contracts/interfaces/ImultisigFactory.sol

--- contracts/interfaces/ImultisigFactory.sol
+++ contracts/interfaces/ImultisigFactory.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.19;
 
 interface IMultisig {
+    struct Transaction {
+        address spender;
+        uint amount;
+        uint numberOfApproval;
+        bool isActive;
+    }
+
     function createTransaction(uint amount, address spender) external;
+
+    function ApproveTransaction(uint _id) external;
+
+    function sendTransaction(uint id) external;
+
+    function getTransaction(uint id) external returns (Transaction memory);
+
+    function calcMinAppoval() external view returns (uint MinAdmin);
 }
