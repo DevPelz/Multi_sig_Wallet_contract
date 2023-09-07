a423d724c85f111e53c7de9f53672189ffcf21c8
Dev_Pelz <pelzfx1@gmail.com>
2023-09-07 12:40:43 +0100

    add: contracts/multisigFactory.sol

---
+++ contracts/multisigFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import "./multisig.sol";
+
+contract MultisigFactory {
+    Multisig[] public wallets;
+
+    function createMultisig(
+        address[] memory _admins
+    ) external returns (Multisig newWallets) {
+        newWallets = new Multisig(_admins);
+        wallets.push(newWallets);
+    }
+
+    function getWallets() external view returns (Multisig[] memory) {
+        return wallets;
+    }
+}
--- scripts/multisig.ts
+++ scripts/multisig.ts
@@ -9,24 +9,36 @@ async function main() {
 
     const amount = ethers.parseEther('5');
     await multisig.waitForDeployment();
+
     console.log(`Multisig deployed to ${multisig.target}`);
     console.log(`======================================`);
 
     const reciept = await multisig.createTransaction(amount, spender.address);
-    console.log((await reciept.wait())?.logs);
+    //@ts-ignore
+    console.log((await reciept.wait())?.logs.args);
 
-    
     await multisig.connect(Admin3).ApproveTransaction(1);
     let balanceBefore = await ethers.provider.getBalance(spender.address);
-    console.log(`Balance Before ${balanceBefore}`);
+
+    console.log(`Balance Before ${ethers.formatEther(balanceBefore)}`);
     console.log(`======================================`); 
-    await multisig.connect(Admin1).ApproveTransaction(1);
-    console.log(`Spender Balance ${ethers.formatEther((await ethers.provider.getBalance(spender.address)) - balanceBefore)}`)
 
+    await multisig.connect(Admin2).ApproveTransaction(1);
+    console.log(`Spender Balance ${ethers.formatEther((await ethers.provider.getBalance(spender.address))- balanceBefore)}`);
+    console.log(`======================================`); 
 
+    console.log(`Multisig balance before: ${ethers.formatEther(await ethers.provider.getBalance(multisig))}`);
+    console.log(`======================================`); 
 
+    const transact = await spender.sendTransaction({from: spender.address, to: multisig.target, value: ethers.parseEther("1")});
+    console.log( await transact.wait());
+    console.log(`======================================`); 
 
+    console.log(`Multisig balance After: ${ethers.formatEther(await ethers.provider.getBalance(multisig))}`);
+    console.log(`======================================`); 
+    // console.log(`sent ${sent} ether to ${multisig.target}`);
 }
+
 
 
 main().catch((error) => {
